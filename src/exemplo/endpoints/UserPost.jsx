16d32f47d6e4ec26923552e22012dc4e73f9a8c1
LincolnLink <link7-7@hotmail.com>
2024-10-26 19:06:41 -0300

    inputs

--- src/exemplo/endpoints/UserPost.jsx
+++ src/exemplo/endpoints/UserPost.jsx
@@ -3,9 +3,41 @@ import './App.css';
   
 const UserPost = () => {  
 
+  const [ username, setUsername ] = React.useState('');
+  const [ email, setEmail ] = React.useState('');
+  const [ password, setPassword ] = React.useState('');
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log({
+
+    });
+
+  }
+
   return (
     <>
       <h1>User Post</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={({target})=> setUsername(target.value)}        
+        />
+        <input 
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={({target})=> setEmail(target.value)}        
+        />
+        <input 
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={({target})=> setPassword(target.value)}        
+        />
+      </form>
     </>
   )
 }
